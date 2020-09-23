5075564c7ed14f1f9e53679afd2dca0266f82d44
jc <jym@jym.sg>
2020-09-23 14:35:32 +0800

    replacing python web service

--- hostclientinstall.sh
+++ hostclientinstall.sh
@@ -7,25 +7,13 @@ PORT=8081
 mkdir -p $HOME/clientinstall
 sleep 10 # wait for sftpconf.zip to appear, it is created within container's scripting
 cp ./backend/sftp/keys/sftpconf.zip $HOME/clientinstall
-# There are alternative ways to define the variable $SFTPCONFURL in a powershell session
 
 cd $HOME/clientinstall
-INSTALLMSG=false
-ver=$(python -c"import sys; print(sys.version_info.major)")
-if [ $ver -eq 2 ]; then
-    INSTALLMSG=true
-    python -m SimpleHTTPServer $PORT > $HOME/clientinstall.log &
-elif [ $ver -eq 3 ]; then
-    INSTALLMSG=true
-    python -m http.server $PORT > $HOME/clientinstall.log &
-else 
-    echo "Please use a webserver to host $HOME/clientinstall files"
-fi
+echo "Please use powershell session with ADMIN rights, copy-&-paste the LAST line\n & press enter to install at Windows endpoints:"
+MSG="\$SFTPCONFURL='http://$IPADDR:$PORT/sftpconf.zip'; Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol $
+echo $MSG > ~/clientinstall/readme.txt
 
-if $INSTALLMSG ; then
-   echo "Please use powershell session with ADMIN rights, copy-&-paste the LAST line\n & press enter to install at Windows endpoints:"
-   echo ""
-   MSG="\$SFTPCONFURL='http://$IPADDR:$PORT/sftpconf.zip'; Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/jymcheong/openedrClient/master/install.ps1'))"
-   echo $MSG 
-   echo $MSG > ~/clientinstall/readme.txt
-fi
+sudo docker run -d -p $PORT:80 -v $PWD/readme.txt:/usr/share/caddy/index.html -v $PWD:/usr/share/caddy -v caddy_data:/data caddy
+
+echo ""
+echo $MSG
