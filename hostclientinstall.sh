e72a129981ab72cfb7523c1de29be477680cf8f2
jc <jym@jym.sg>
2020-09-23 14:50:43 +0800

    Update hostclientinstall.sh

--- hostclientinstall.sh
+++ hostclientinstall.sh
@@ -10,7 +10,7 @@ cp ./backend/sftp/keys/sftpconf.zip $HOME/clientinstall
 
 cd $HOME/clientinstall
 echo "Please use powershell session with ADMIN rights, copy-&-paste the LAST line\n & press enter to install at Windows endpoints:"
-MSG="\$SFTPCONFURL='http://$IPADDR:$PORT/sftpconf.zip'; Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol $
+MSG="\$SFTPCONFURL='http://$IPADDR:$PORT/sftpconf.zip'; Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/jymcheong/openedrClient/master/install.ps1'))"
 echo $MSG > ~/clientinstall/readme.txt
 
 sudo docker run -d -p $PORT:80 -v $PWD/readme.txt:/usr/share/caddy/index.html -v $PWD:/usr/share/caddy -v caddy_data:/data caddy
