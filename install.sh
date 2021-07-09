b03911edd184e4dd1aad24a4d6a0c1664999fd6a
Jym Cheong <jymcheong@icloud.com>
2021-07-09 09:29:46 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,22 @@
 #!/bin/bash
-curl -L https://github.com/jymcheong/OpenEDR/tarball/master | tar xz && mv jymcheong* openEDR && cd openEDR && ./installcontainers.sh
+
+if ! command -v curl &> /dev/null
+then
+    echo "curl is missing... pls install in order to proceed further"
+    exit
+fi
+
+if ! command -v docker &> /dev/null
+then
+    echo "docker is missing... pls install in order to proceed further"
+    exit
+fi
+
+if ! command -v docker-compose &> /dev/null
+then
+    echo "docker-compose is missing... pls install in order to proceed further"
+    exit
+fi
+
+curl -L https://github.com/jymcheong/OpenEDR/tarball/master | tar xz && mv jymcheong* openEDR
+cd openEDR && ./installcontainers.sh
