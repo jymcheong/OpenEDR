1716a41b4842b218c769cb8073f2f65d97496820
Jym Cheong <11232021+jymcheong@users.noreply.github.com>
2021-08-19 09:11:06 +0800

    pre-process 4688 n 4689 events

--- backend/insertEvent.js
+++ backend/insertEvent.js
@@ -69,8 +69,11 @@ function processFile(filepath) {
         .pipe(es.split())
         .pipe(es.mapSync(async function(line) {            
             s.pause(); // process line next and call s.resume() when rdy
+            if(line.length > 0){
+                line = await preProcess(line)            
+                processLine(line, org) // resume the readstream, possibly from a callback
+            } 
             //DO NOT use await for processLine
-            processLine(line, org) // resume the readstream, possibly from a callback
             s.resume();
         })
         .on('error', function(err){
@@ -156,4 +159,54 @@ async function startCleanUp(){
             
         }
     },30000);
+}
+
+/**
+ * Pre-process message to extract fields from 4688 & 4689 Message fields.
+ * Note that Nxlog is not extracting the fields & ODB-AddEvent function removes Message field due to data-deduplication
+ * 
+ * @param {string} msg JSON event message
+ * @returns 
+ */
+ async function preProcess(msg) {
+    return new Promise((resolve, reject) => {
+      try{
+        let event = JSON.parse(msg)
+        // Windows audit event 4688 & 4689 are generated ahead of respective Sysmon events
+        if((event.EventID == 4688 || event.EventID == 4689) && event.Channel == 'Security') {      
+          let msg468X = event.Message.split('\r\n')
+          for(var i=3; i< msg468X.length; i++) {            
+            let match468X = msg468X[i].match(/\t(.+)\:\t+(.+)/mi)
+            if(match468X == null) continue;
+            if(match468X.length == 3) {                
+                if (typeof match468X[1].replaceAll !== "undefined") { 
+                // need to de-duplicate, otherwise ODB insert will fail
+                    let key = match468X[1].replaceAll(' ','')
+                    if(key in event) continue
+                    event[key] = match468X[2]
+                }
+            }
+          }
+          
+          if(event.EventID == 4688){
+            let parentPID = event.Message.match(/\s+Creator Process ID\:\s+(.+)\s+/mi)
+            if(parentPID.length > 1) event.PPID = parseInt(parentPID[1])
+            
+            let NewPID = event.Message.match(/\s+New Process ID\:\s+(.+)\s+/mi)
+            if(NewPID.length > 1) event.PID = parseInt(NewPID[1]) 
+          }
+          if(event.EventID == 4689){
+            let PID = event.Message.match(/\s+Process ID\:\s+(.+)\s+/mi)
+            if(PID.length > 1) event.PID = parseInt(PID[1])
+          }
+        }
+        // we added new fields to json object, need to return stringify
+        //console.log(JSON.stringify(event))
+        msg = JSON.stringify(event)
+        resolve(msg)
+      }  
+      catch(error){
+        reject(error)
+      }
+    })
 }
